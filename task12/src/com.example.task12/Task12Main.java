9708a63579551081ee31f81f48b4f12a4cece7b7
Deeplerg <deeplerg@gmail.com>
2024-09-13 00:44:44 +0500

    Complete task 12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,35 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(arr == null || arr.length == 0)
+            return;
+
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = findIndexOfMinStartingFromIndex(arr, i);
+            swap(arr, i, minIndex);
+        }
     }
 
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+
+    private static int findIndexOfMinStartingFromIndex(int[] array, int fromIndex) {
+        int minIndex = fromIndex;
+        int minValue = array[fromIndex];
+
+        for (int i = fromIndex + 1; i < array.length; i++) {
+            int element = array[i];
+
+            if(element <= minValue) {
+                minValue = element;
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
 }
