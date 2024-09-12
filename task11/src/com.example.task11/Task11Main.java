b51c7cb0243b5617e2d71b2c3e1f3bd278e1701e
Deeplerg <deeplerg@gmail.com>
2024-09-13 00:44:40 +0500

    Complete task 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,31 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(arr == null || arr.length == 0)
+            return;
+
+         int minIndex = numMin(arr);
+
+         int temp = arr[minIndex];
+         arr[minIndex] = arr[0];
+         arr[0] = temp;
     }
 
+    // I could also just do this:
+    // int minIndex = Task10Main.numMin(arr)
+    private static int numMin(int[] arr) {
+        int minIndex = 0;
+        int minValue = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            int element = arr[i];
+
+            if(element <= minValue) {
+                minValue = element;
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
 }
